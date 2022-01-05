c24ef7929ea69c547cf7b04dfaa5bae67377e6b5
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-01-05 14:52:19 -0600

    3rd Commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
  //  Check now, comment out later
 console.log("DOM fully loaded and parsed"); 
 });
+
+
 // Add image cards oject arrays
 const cardArr = [
 {
@@ -54,24 +56,38 @@ const cardArr = [
     img: 'image/slice_cake.jpg'
 }         
 ]
-// Try both querySelector and getElementById, set to grid
-let grid = document.querySelector('.grid')
 
-// Create grid game board
+// Try both querySelector and getElementById (both works), set to = grid div
+// let grid = document.querySelector('.grid')
+let grid = document.getElementById('grid')
+// Create empty array for chosen cards
+let cardsChosen= []
+
+
+// Create grid game board function
 function gameBoard() {
+    // create for loop, to loop thru cards 
     for (let i=0; i < cardArr.length; i++){
         let card = document.createElement('img')
+        // let card to img, set to cottoncandy img
         card.setAttribute('src', 'image/cottoncandy.jpg')
         card.setAttribute('data-id', i)
         // Invoke a card flip
-        // card.addEventListener('click', 'flipcard')
+        // card.addEventListener('click', 'cardFlip')
         grid.appendChild(card)
     }
 }
 gameBoard()
 
 
+// Check card match function
 
+// Card flip function
+function cardFlip (){
+    // Get Id from above
+    let cardId = this.getAttribute('data-id')
+
+}
 
 
 
