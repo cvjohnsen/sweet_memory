ad740f44f16845aa7b70248374e1e7e9af1f5fcc
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-01-07 10:59:27 -0600

    7th Commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 document.addEventListener('DOMContentLoaded', () => {
  //  Check now, comment out later
 // console.log("DOM fully loaded and parsed"); 
-
 // Add image cards oject arrays
 const cardArr = [
 {
@@ -54,21 +53,16 @@ const cardArr = [
     img: 'image/slice_cake.jpg'
 }         
 ]
-
-
 // Restart and randomize cards 
 cardArr.sort(() => 0.5 - Math.random())
-
 // Try both querySelector and getElementById (both works), set to = grid div
 // let grid = document.querySelector('.grid')
-const grid = document.querySelector('#grid')
-const resultDisplay = document.querySelector('#result')
+const grid = document.getElementById('grid')
+const resultDisplay = document.getElementById('results')
 // Create empty array for chosen cards, Id, won
 let cardsChosen = []
 let cardsChosenId = []
 let cardsWon = []
-
-
 // Create grid game board function
 function gameBoard() {
     // create for loop, to loop thru cards 
@@ -77,45 +71,42 @@ function gameBoard() {
         // let card to img, set to sweet img
         card.setAttribute('src', 'image/sweet-500.jpg')
         card.setAttribute('data-id', i)
-        // Invoke a card flip
-        card.addEventListener('click', flipCard)
+        // Invoke a card flip function
+        card.addEventListener('click', cardFlip)
         grid.appendChild(card)
     }
 }
-
-
 // Check card match function
 function cardMatch(){
     const cards = document.querySelectorAll('img')
-    const card1Id= cardsChosenId[0]
-    const card2Id= cardsChosenId[1]
+    const card1Id = cardsChosenId[0]
+    const card2Id = cardsChosenId[1]
     // Choose Cards 1 & 2
-    if (card1Id === card2Id) {
-        cards[card1Id].setAttribute('src', 'image/cottoncandy.jpg')
-        cards[card2Id].setAttribute('src', 'image/cottoncandy.jpg')
-        alert('You found a pair!')
-    }else if (card1Id === card2Id) {
+    if (cardsChosenId[0] === cardsChosenId[1]) {
+        cards[card1Id].setAttribute('src', 'image/white.jpg')
+        cards[card2Id].setAttribute('src', 'image/white.jpg')
+        alert('Match!')
+    }else if (cardsChosenId[0] === cardsChosenId[1]) {
         alert('You found a match!')
-        cards[card1Id].setAttribute('src' 'image/cottoncandy.jpg')
-        cards[card2Id].setAttribute('src' 'image/cottoncandy.jpg')
+        cards[card1Id].setAttribute('src', 'image/white.jpg')
+        cards[card2Id].setAttribute('src', 'image/white.jpg')
+        // remove cards
         cards[card1Id].removeEventListener('click', cardFlip)
         cards[card2Id].removeEventListener('click', cardFlip)
         cardsWon.push(cardsChosen)
     } else {
-        cards[card1Id].setAttribute('src' 'image/cottoncandy.jpg')
-        cards[card2Id].setAttribute('src' 'image/cottoncandy.jpg')
+        cards[card1Id].setAttribute('src', 'image/white.jpg')
+        cards[card2Id].setAttribute('src', 'image/white.jpg')
         alert('Try again!') 
     }
     cardsChosen = []
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
     // Only flip 2
-    if (cardsWon.length === cardArr.length/2){
+    if (cardsWon.length === cardArr.length /2) {
         resultDisplay.textContent = 'Yay, You Won!'
     }
 }
-
-
 // Card flip function
 function cardFlip (){
     // Get Id from above
@@ -123,11 +114,36 @@ function cardFlip (){
     cardsChosen.push(cardArr[cardId].name)
     cardsChosenId.push(cardId)
     this.setAttribute('src', cardArr[cardId].img)
-    if(cardsChosen.length === 2) {
+    if (cardsChosen.length === 2) {
         // Check match and wait
-        setTimeout(checkForMatch, 600)
+        setTimeout(cardMatch, 500)
     }
 }
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0;
+            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var time = 60 / 2, // your time in seconds here
+        display = document.querySelector('#safeTimerDisplay');
+    startTimer(time, display);
+};
+
 
 gameBoard()
 });
