d4959f663fde6888f9ceb0063e43ef427b489f3f
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-01-10 00:42:21 -0600

    8th Commit

--- app.js
+++ app.js
@@ -55,14 +55,17 @@ const cardArr = [
 ]
 // Restart and randomize cards 
 cardArr.sort(() => 0.5 - Math.random())
+
 // Try both querySelector and getElementById (both works), set to = grid div
 // let grid = document.querySelector('.grid')
 const grid = document.getElementById('grid')
-const resultDisplay = document.getElementById('results')
+const resultDisplay = document.getElementById('result')
+
 // Create empty array for chosen cards, Id, won
 let cardsChosen = []
 let cardsChosenId = []
 let cardsWon = []
+
 // Create grid game board function
 function gameBoard() {
     // create for loop, to loop thru cards 
@@ -79,34 +82,36 @@ function gameBoard() {
 // Check card match function
 function cardMatch(){
     const cards = document.querySelectorAll('img')
-    const card1Id = cardsChosenId[0]
-    const card2Id = cardsChosenId[1]
+    const pick1Id = cardsChosenId[0]
+    const pick2Id = cardsChosenId[1]
     // Choose Cards 1 & 2
-    if (cardsChosenId[0] === cardsChosenId[1]) {
-        cards[card1Id].setAttribute('src', 'image/white.jpg')
-        cards[card2Id].setAttribute('src', 'image/white.jpg')
+    if (pick1Id === pick2Id) {
+        cards[pick1Id].setAttribute('src', 'image/sweet-500.jpg')
+        cards[pick2Id].setAttribute('src', 'image/sweet-500.jpg')
         alert('Match!')
-    }else if (cardsChosenId[0] === cardsChosenId[1]) {
+    }else if (cardsChosen[0] === cardsChosen[1]) {
         alert('You found a match!')
-        cards[card1Id].setAttribute('src', 'image/white.jpg')
-        cards[card2Id].setAttribute('src', 'image/white.jpg')
+        cards[pick1Id].setAttribute('src', 'image/sweet-500.jpg')
+        cards[pick2Id].setAttribute('src', 'image/sweet-500.jpg')
         // remove cards
-        cards[card1Id].removeEventListener('click', cardFlip)
-        cards[card2Id].removeEventListener('click', cardFlip)
+        cards[pick1Id].removeEventListener('click', cardFlip)
+        cards[pick2Id].removeEventListener('click', cardFlip)
         cardsWon.push(cardsChosen)
     } else {
-        cards[card1Id].setAttribute('src', 'image/white.jpg')
-        cards[card2Id].setAttribute('src', 'image/white.jpg')
+        cards[pick1Id].setAttribute('src', 'image/sweet-500.jpg')
+        cards[pick2Id].setAttribute('src', 'image/sweet-500.jpg')
         alert('Try again!') 
     }
     cardsChosen = []
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
     // Only flip 2
-    if (cardsWon.length === cardArr.length /2) {
+    if (cardsWon.length === cardArr.length/2) {
         resultDisplay.textContent = 'Yay, You Won!'
     }
 }
+
+
 // Card flip function
 function cardFlip (){
     // Get Id from above
@@ -116,34 +121,35 @@ function cardFlip (){
     this.setAttribute('src', cardArr[cardId].img)
     if (cardsChosen.length === 2) {
         // Check match and wait
-        setTimeout(cardMatch, 500)
+        setTimeout(cardMatch, 600)
     }
 }
 
+
+// Add game timer
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
+    let timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
-
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
         display.textContent = minutes + ":" + seconds;
-
         if (--timer < 0) {
             timer = 0;
-            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+            timer = duration; // uncomment this line to reset timer automatically after reaching 0
         }
     }, 1000);
 }
-
 window.onload = function () {
-    var time = 60 / 2, // your time in seconds here
+    let time = 60 / 2, // your time in seconds here
         display = document.querySelector('#safeTimerDisplay');
     startTimer(time, display);
 };
 
+// Add start/reset button?
+// document.getElementById('start').click = startTimer;
 
 gameBoard()
 });
+
